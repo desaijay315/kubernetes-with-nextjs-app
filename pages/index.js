1023c123d0a4ed3723b827a12a939387286d32c7
desaijay315 <desaijay315@gmail.com>
2020-09-29 22:03:51 +0530

    nextjs with kubernetes final step

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Nextjs with Kubernetes</a>
+          <a href="https://nextjs.org">Nextjs with Kubernetes</a>
         </h1>
 
         <p className={styles.description}>
