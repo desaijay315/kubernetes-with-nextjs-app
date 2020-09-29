0301816aab9666b2339d314d11ab33e99525eacf
desaijay315 <desaijay315@gmail.com>
2020-09-29 21:59:55 +0530

    nextjs with kubernetes final step

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App with Kubernetes</title>
+        <title>Nextjs with Kubernetes</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Nextjs with Kubernetes</a>
         </h1>
 
         <p className={styles.description}>
